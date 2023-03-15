4484f05c572fcb044cc1e8d7f7bb7fd7d9dac497
Dylan Eustice <deustice@nvidia.com>
2023-03-14 21:32:52 -0400

    Start Predictor classes

--- src/objects.py
+++ src/objects.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Fields:
   def __init__(self, base_fields):
     self.base   = base_fields
@@ -7,3 +9,21 @@ class Fields:
     self.Tb_avg = [f'Tb{f}' for f in self.foravg] + [f'Tb{f}' for f in self.oppavg]
     self.win    = [f'W{f}' for f in base_fields]
     self.loss   = [f'L{f}' for f in base_fields]
+  
+class Predictor:
+  def __init__(self, input_fields, output_fields):
+    self.input_fields = input_fields
+    self.output_fields = output_fields
+    self.model = None
+  
+  def train(self):
+    return None
+  
+  def predict(self):
+    return None
+
+class LeastSquares(Predictor):
+  def train(self, df):
+    A = df[self.input_fields]
+    b = df[self.output_fields]
+    self.model = np.linalg.lstsq(A, b)
