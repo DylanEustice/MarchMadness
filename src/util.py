71d3ca6bf5a845c2c45950597d85163f757af2a6
Dylan Eustice <deustice@nvidia.com>
2023-03-14 21:32:57 -0400

    Indentation

--- src/util.py
+++ src/util.py
@@ -12,11 +12,11 @@ Load in a CSV data file, dropping any seasons before 'first_season' and any
 corresponding to seasons in 'drop_seasons'
 '''
 def load_csv(
-    filename,
-    first_season=constants.first_season,
-    drop_seasons=constants.drop_seasons,
-    data_path=constants.data_path
-  ):
+  filename,
+  first_season=constants.first_season,
+  drop_seasons=constants.drop_seasons,
+  data_path=constants.data_path
+):
   df = pd.read_csv(os.path.join(data_path, filename))
   if 'Season' in df.columns:
     df.drop(df[df['Season'] < first_season].index, inplace=True)
@@ -24,15 +24,32 @@ def load_csv(
       df.drop(df[df['Season'] == seas].index, inplace=True)
   return df.reset_index(drop=True)
 
+def save_csv(df, filename, data_path=constants.data_path):
+  df.to_csv(os.path.join(data_path, filename), index=False)
+
 '''
 Load teams, adding conference ID
 '''
-def load_teams(data_path=constants.data_path):
+def load_teams(
+  first_season=constants.first_season,
+  drop_seasons=constants.drop_seasons,
+  data_path=constants.data_path
+):
   # Make a conference dictionary
-  conf_df = load_csv('Conferences.csv', data_path=data_path)
+  conf_df = load_csv(
+    'Conferences.csv',
+    first_season=first_season,
+    drop_seasons=drop_seasons,
+    data_path=data_path
+  )
   conf_df['ConfID'] = conf_df.index
   conf_dict = dict(zip(conf_df.ConfAbbrev, conf_df.ConfID))
   # Add conference ID's and return
-  team_df = load_csv('MTeamConferences.csv', data_path=data_path)
+  team_df = load_csv(
+    'MTeamConferences.csv',
+    first_season=first_season,
+    drop_seasons=drop_seasons,
+    data_path=data_path
+  )
   team_df['ConfId'] = list(map(conf_dict.get, team_df['ConfAbbrev']))
   return team_df
